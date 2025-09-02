3bc09f1337bf0e20e5a440b55390ba8db9c92cd2
michaelrichie95 <michaelrichie95@yahoo.com>
2025-09-02 11:28:53 -0400

    Update DoctorRepository.java

--- app/src/main/java/com/project/back_end/repo/DoctorRepository.java
+++ app/src/main/java/com/project/back_end/repo/DoctorRepository.java
@@ -1,12 +1,11 @@
 package com.project.back_end.repo;
 
 import com.project.back_end.models.Doctor;
-import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
